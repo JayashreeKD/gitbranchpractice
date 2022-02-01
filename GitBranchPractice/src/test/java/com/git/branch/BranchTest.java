c3d75f444d6e5c8c4ed985756713e0d08dd9fa50
JayashreeKD <jayashreek71@gmail.com>
2022-02-01 11:08:31 +0530

    my first comit from gitbrachpractice

---
+++ GitBranchPractice/src/test/java/com/git/branch/BranchTest.java
@@ -0,0 +1,15 @@
+package com.git.branch;
+
+import org.testng.annotations.Test;
+
+public class BranchTest {
+	
+	@Test
+	public void branchTest()
+	{
+		System.out.println("insde the branch ");
+		
+	}
+
+
+}
